f5febf1da3267f6f86a569879ed1afff135b4903
Bahaa Ehab El Dien Abdelmawla <mbcompany888@gmail.com>
2020-12-19 01:09:52 +0200

    start

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8c68477267f9a17d57d00288be38a277'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '0e5f2a36-5371-44a5-96fc-e87eb124dbc3'
+};
